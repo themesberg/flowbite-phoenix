0a65c899c0c87e30d5f22f9b87c49943e1b2c22f
Matt Van Horn <mattvanhorn@gmail.com>
2024-05-03 11:53:41 -0700

    One more hero block

--- lib/flowbite_phoenix_web/components/blocks/marketing/hero_components.ex
+++ lib/flowbite_phoenix_web/components/blocks/marketing/hero_components.ex
@@ -2,6 +2,7 @@ defmodule FlowbitePhoenixWeb.Blocks.Marketing.HeroComponents do
   use Phoenix.Component
 
   import FlowbitePhoenixWeb.Elements
+  import FlowbitePhoenixWeb.Icons.Arrows
 
   slot(:headline)
   slot(:subhead)
@@ -54,6 +55,47 @@ defmodule FlowbitePhoenixWeb.Blocks.Marketing.HeroComponents do
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
           <%= render_slot(@inner_block) %>
+        </div>
+      </div>
+    </section>
+    """
+  end
+
+  attr(:href, :string)
+  slot(:headline, required: true)
+  slot(:inner_block)
+
+  @spec hero_cta(map()) :: Phoenix.LiveView.Rendered.t()
+  def hero_cta(assigns) do
+    ~H"""
+    <div>
+      <h2 class="mb-1 text-lg font-bold"><%= render_slot(@headline) %></h2>
+      <p class="mb-1 text-sm font-medium text-gray-400"><%= render_slot(@inner_block) %></p>
+      <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+          Read more
+          <.arrow_right class="ml-1 w-5 h-5" />
+      </a>
+    </div>
+    """
+  end
+
+  attr(:bg_image, :string, required: true)
+  slot(:headline, required: true)
+  slot(:ctas)
+  slot(:inner_block)
+
+  def bg_cover_img_w_ctas(assigns) do
+    ~H"""
+    <section style={"--image-url: url(#{@bg_image})"} class={"bg-[image:--image-url] bg-no-repeat bg-cover bg-center bg-gray-700 bg-blend-multiply"}>
+      <div class="relative py-8 px-4 mx-auto max-w-screen-xl text-white lg:py-16 z-1">
+        <div class="mb-6 max-w-screen-lg lg:mb-0">
+          <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">
+            <%= render_slot(@headline) %>
+          </h1>
+          <%= render_slot(@inner_block) %>
+        </div>
+        <div class="grid gap-8 pt-8 lg:pt-12 mt-8 lg:mt-12 border-t border-gray-600 sm:grid-cols-2 lg:grid-cols-4">
+          <%= render_slot(@ctas) %>
         </div>
       </div>
     </section>
